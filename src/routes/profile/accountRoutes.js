a5e46083ba1c7d402db8aaa244040211547904fd
Karl Brycz <kbrycz@umich.edu>
2024-03-10 11:45:45 -0500

    chaning route

--- src/routes/profile/accountRoutes.js
+++ src/routes/profile/accountRoutes.js
@@ -1,105 +1,102 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const User = mongoose.model('User')
-const requireAuth = require('../../middlewares/requireAuth')
-const router = express.Router()
+const express = require('express');
+const mongoose = require('mongoose');
+const User = mongoose.model('User');
+const requireAuth = require('../../middlewares/requireAuth');
+const router = express.Router();
 
-// Changes the name of the user and returns user object to user
+// Utility for logging and constructing error response
+const logAndRespond = (err, res) => {
+    console.error(err); // Log the error for server-side debugging
+    res.status(422).send({ error: err.message });
+};
+
+// Changes the name of the user and returns user object to the user
 router.post('/changeName', requireAuth, async (req, res) => {
-    console.log("Inside change name")
-    console.log(req.body)
-    const {first, last} = req.body
+    const { first, last } = req.body;
 
     if (!first || !last) {
-        console.log("Error with first and last names passed in")
-        return res.status(422).send({error: "Error with first and last names passed in"})
+        return res.status(422).send({ error: "Error with first and last names passed in" });
     }
+
     try {
-        console.log("insdie try")
-        console.log(req.user)
-        User.findOne({_id: req.user._id}, (err, user) => {
-            user.first = first
-            user.last = last
-            console.log("sssssss")
-            user.save(() => {
-                console.log("User updated first and last name")
-                return res.send({user})
-            })
-        })
+        const user = await User.findOne({ _id: req.user._id });
+        if (!user) {
+            return res.status(404).send({ error: "User not found" });
+        }
+
+        user.first = first;
+        user.last = last;
+        await user.save();
+        res.send({ user });
+    } catch (err) {
+        logAndRespond(err, res);
     }
-    catch (err) {
-        console.log(err)
-        return res.status(422).send({error: err.message})
-    }
-})
+});
 
 // Changes the birthdate of the user and returns the user object to user
 router.post('/changeBirthdate', requireAuth, async (req, res) => {
-    const {birthdate} = req.body
+    const { birthdate } = req.body;
 
     if (!birthdate) {
-        console.log("Error with changing birthdate")
-        return res.status(422).send({error: 'Error with changing birthdate'})
+        return res.status(422).send({ error: 'Error with changing birthdate' });
     }
+
     try {
-        User.findOne({_id: req.user._id}, (err, user) => {
-            user.birthdate = birthdate
-            user.save(() => {
-                console.log("User updated birthdate info")
-                return res.send({user})
-            })
-        })
+        const user = await User.findOne({ _id: req.user._id });
+        if (!user) {
+            return res.status(404).send({ error: "User not found" });
+        }
+
+        user.birthdate = new Date(birthdate);
+        await user.save();
+        res.send({ user });
+    } catch (err) {
+        logAndRespond(err, res);
     }
-    catch (err) {
-        return res.status(422).send({error: err.message})
-    }
-})
+});
 
 // Changes the gender of the user
 router.post('/changeGender', requireAuth, async (req, res) => {
-    const { gender } = req.body
+    const { gender } = req.body;
 
-    if (gender < 0) {
-        console.log("Error with gender variable passed to server")
-        return res.send({user: req.user})
+    if (gender === undefined) {
+        return res.status(422).send({ error: "Error with gender variable passed to server" });
     }
+
     try {
-        User.findOne({_id: req.user._id}, (err, user) => {
-            user.gender = gender
-            user.save(() => {
-                console.log("User updated gender info")
-                return res.send({user})
-            })
-        })
+        const user = await User.findOne({ _id: req.user._id });
+        if (!user) {
+            return res.status(404).send({ error: "User not found" });
+        }
+
+        user.gender = gender;
+        await user.save();
+        res.send({ user });
+    } catch (err) {
+        logAndRespond(err, res);
     }
-    catch (err) {
-        console.log("Unable to update gender info: " + err.message)
-        return res.status(422).send({error: err.message})
-    }
-})
+});
 
 // Changes the notification settings of the user
 router.post('/changeNotifications', requireAuth, async (req, res) => {
-    const { notifications } = req.body
+    const { notifications } = req.body;
 
     if (notifications === null) {
-        console.log("error with notifications variable")
-        return res.status(422).send({error: 'Must provide notifications'})
+        return res.status(422).send({ error: 'Must provide notifications' });
     }
+
     try {
-        User.findOne({_id: req.user._id}, (err, user) => {
-            user.notifications = notifications
-            user.save(() => {
-                console.log("User updated notifications object")
-                return res.send({user})
-            })
-        })
-    }
-    catch (err) {
-        console.log("Error with changing the notifications: " + err.message)
-        return res.status(422).send({error: err.message})
-    }
-})
+        const user = await User.findOne({ _id: req.user._id });
+        if (!user) {
+            return res.status(404).send({ error: "User not found" });
+        }
 
+        user.notifications = notifications;
+        await user.save();
+        res.send({ user });
+    } catch (err) {
+        logAndRespond(err, res);
+    }
+});
 
-module.exports = router
+module.exports = router;
